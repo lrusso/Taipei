264a9c9f36deb91a36131aaa68ff0750939c0030
Leonardo Russo <info@lrusso.com>
2020-02-20 00:31:17 -0300

    Add files via upload

--- TaipeiGame.js
+++ TaipeiGame.js
@@ -442,7 +442,7 @@ Taipei.Game.prototype = {
 				{
 				var toastShadow = game.add.graphics();
 				toastShadow.beginFill(0x000000, 0.4);
-				var toastText = game.add.text(0, 0, "Game Over", { font: "bold 24px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" });
+				var toastText = game.add.text(0, 0, "No free tiles", { font: "bold 24px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" });
 				toastText.setShadow(3, 3, "rgba(0,0,0,0.5)", 2);
 				toastText.setTextBounds(0, 380, 800, 55);
 				toastShadow.drawRoundedRect(800 / 2 - toastText._width / 2 - 11, 383, toastText._width + 23, 46, 10);
