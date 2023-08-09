45c10d3a2f9f2d9ca8218b84dca575270ce91777
Leonardo Russo <info@lrusso.com>
2023-08-09 12:51:41 -0300

    Add files via upload

--- TaipeiGame.js
+++ TaipeiGame.js
@@ -181,6 +181,9 @@ Taipei.Splash.prototype = {
 
 	create: function()
 		{
+		// HIDING THE LOADING SPINNER
+		document.getElementById("loading").style.display = "none";
+
 		// SETTING THE LOGO OFFSET
 		var logoOffset = 20;
 
